f81b1c0aa98f0fcd44bf235b8bc1493f80c770d3
kyileeyu <kyileeeyu@gmail.com>
2021-10-06 20:32:01 +0900

    Modify:checkout

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,41 @@
 <template>
-  <div class="app">
-    <div class="not-logged-in" v-if="login == 0 ">
-      <img src="./assets/avocado.png"/>
-      <p>
-        가장 : 가볍게 장보자<br/>
-        물건사러 가장!
-      </p>
-      <router-link to="/Register" class="btn">회원가입 하기</router-link>
-      <router-link to="/Login" class="btn">로그인 하기</router-link>
-      <router-view/>
+<div class="app">
+    <div v-if="step == 0">
+      <!-- <HelloWorld /> -->
     </div>
-    <div class="logged-in" v-if="login ==1">
-      <div v-if="step == 0">
-        <!-- <HelloWorld /> -->
-        <Register />
-        <Login />
-      </div>
-      <div v-if="step == 2">
-        <PageDetail />
-      </div>
+    
+    <div v-if="step == 2">
+      <PageDetail />
+    </div>
+    
+    <div v-if="step == 1">
+      <Posting />
+    </div>
+
+
+    <footer>
+
+      <button class="home">
+        <img class="home-icon" src="./assets/home.png"/>
+        장보기
+      </button>
+
+      <button class="post" @click=" step=2 ">
+        <img class="post-icon" src="./assets/post.png"/>
+        글쓰기
+      </button>
+
+      <button class="chat" @click=" step=1" >
+        <img class="chat-icon" src="./assets/chat.png"/>
+        채팅
+      </button>
+
+      <button class="mypage">
+        <img class="mypage-icon" src="./assets/user.png"/>
+        내정보
+      </button>
       
-      <div v-if="step == 1">
-        <Posting />
-      </div>
-      <div v-if="step == 3">
-        <Mypage />
-      </div>
-
-
-      <footer>
-        <button class="home" @click=" step=0">
-          <img class="home-icon" src="./assets/home.png"/>
-          장보기
-        </button>
-
-        <button class="post" @click=" step=1 ">
-          <img class="post-icon" src="./assets/post.png"/>
-          글쓰기
-        </button>
-
-        <button class="chat" @click=" step=2" >
-          <img class="chat-icon" src="./assets/chat.png"/>
-          채팅
-        </button>
-
-        <button class="mypage" @click=" step=3">
-          <img class="mypage-icon" src="./assets/user.png"/>
-          내정보
-        </button>
-      </footer>
-    </div>
+    </footer>
   </div>
 </template>
 
@@ -57,32 +43,23 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import PageDetail from './components/PageDetail.vue'
 import Posting from './components/Posting.vue'
-import Mypage from './components/Mypage.vue'
-import Register from './components/Register.vue'
-import Login from './components/Login.vue'
 import './style/reset.css'
-
-
 export default {
   name: 'App',
   data(){
     return{
-      login : 0,
       step : 0,
     }
-
   },
   components: {
     // HelloWorld,
     PageDetail,
     Posting,
-    Mypage,
-    Register,
-    Login,
   },
   props:{
-  },
+  }
 }
+
 </script>
 
 <style lang="scss">
@@ -91,65 +68,38 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-.not-logged-in{
-  margin : 100px auto;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  img{
-    margin-bottom: 30px;
-    width: 100px;
-    height: 100px;
-    overflow: hidden;
-  }
-  p{
-    font-weight: 700;
-    margin-bottom: 50px;
-  }
-  .btn{
-    width:80%;
-    max-width: 280px;
-    margin: 10px 0;
-  }
-}
-.logged-in{
   margin-top: 60px;
-  
-  }
-
- footer{
+}
+footer{
     display: flex;
     justify-content: space-around;
     align-items: center;
-
     border-top: solid 1px #eee;
     padding : 5px 0;
     background-color: #fff;
-
     position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
-
   }
-
   footer button{
     width: 15%;
     height: 15%;
-
     font-size: 5px;
   }
-
   footer img{
     display: block;
     margin: 0 auto;
     width: 15px;
     height: 15px;
   }
-
   .content {
     text-align: left;
   }
 </style>
+
+
+
+
+
+
